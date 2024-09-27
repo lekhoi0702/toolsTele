bbc3ee92c083bf9a876e691b5b3e3941594487e3
Le Tuan Khoi <93958255+lekhoi0702@users.noreply.github.com>
2024-09-27 13:47:55 +0700

    moon-bix

---
+++ node/moonbix-bot/app/config/config.js
@@ -0,0 +1,5 @@
+export class Config {
+  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 
+  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: ""
+  static RUNMODE = 2; // 1 FOR 1 BY 1 RUN & 2 FOR MASS RUN
+}
---
+++ node/moonbix-bot/config/proxy_list.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Example : 
+ * export const proxyList = [
+    "http://tsjc0u7c:tSjC0u7c@103.187.4.134:48151",
+    "http://root:rZeE9x0a@103.74.102.107:62972",
+    "http://2GgJdGSax:1rtqZ28MY@87.120.216.140:63882",
+  ];
+ */
+
+export const proxyList = [
+  "http://fh46u7s:sdf9854@103.179.184.236:2747",
+  "http://fh46u7s:sdf9854@103.179.184.236:2747",
+  "http://fh46u7s:sdf9854@103.179.184.236:2747"
+
+];
